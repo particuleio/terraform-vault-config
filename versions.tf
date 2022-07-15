b4dc4dc6e43f34c662513bd36c9d1619a3237607
Kevin Lefevre <kevin@particule.io>
2022-07-15 12:14:51 +0200

    fix
    Signed-off-by: Kevin Lefevre <kevin@particule.io>

--- versions.tf
+++ versions.tf
@@ -1,1 +1,9 @@
-# Specify required provider versions below
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = ">= 3.7"
+    }
+  }
+}
